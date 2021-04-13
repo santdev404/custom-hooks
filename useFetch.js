b2a45988d7f9c36736c4f30d00913dc2d2040cb9
Santdev404 <santdev404@gmail.com>
2021-04-13 12:25:18 -0500

    Custom hooks

---
+++ useFetch.js
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from "react"
+
+
+export const useFetch = (url) => {
+
+
+    const isMounted = useRef(true);
+
+    const [state, setState] = useState({
+        data: null,
+        loading: true,
+        error: null
+    })
+
+
+    useEffect(() => {
+
+        return () =>{
+            isMounted.current = false;
+        }
+
+    }, [])
+
+    useEffect(() => {
+
+        setState({
+            data: null,
+            loading: true,
+            error: null
+        })
+
+
+        
+        fetch(url)
+            .then( resp => resp.json())
+            .then( data => {
+
+                if( isMounted.current){
+                                        
+                    setState({
+                        loading: false,
+                        error: null,
+                        data
+                    })  
+                    
+                }else{
+                    console.log('SetState no se llamo')
+                }
+
+            });
+        
+    }, [url])
+
+    return state;
+
+}
